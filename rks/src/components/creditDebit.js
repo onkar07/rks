838b63df559fbf6d9cff6ac75745449c8f4d826a
onkar07 <34215437+onkar07@users.noreply.github.com>
2022-01-07 12:43:32 +0530

    debit credit logic
    debit credit logic done design remaning

--- rks/src/components/creditDebit.js
+++ rks/src/components/creditDebit.js
@@ -2,14 +2,17 @@ import React, { useRef, useState } from 'react'
 import {db} from '../config/firebase';
 import './creditDebit.css'
 import {getDocs, collection, addDoc, onSnapshot} from 'firebase/firestore'
-import { query, orderBy, limit } from "firebase/firestore";  
+import { query, orderBy, limit, getDocuments } from "firebase/firestore";  
 function CreditDebit() {
+  const [select, setSelect] = useState("debit")
   var currentTimeInSeconds=Math.floor(Date.now()/1000);
   const type = useRef();
   const subject = useRef();
   const amount = useRef();
   var bal;
-  const [lastRec, setLastRec] = useState();
+  var comTime = [];
+  var recArray = []
+  var lastRec = [];
   var records = {
     time : currentTimeInSeconds,
     type : "",
@@ -19,15 +22,20 @@ function CreditDebit() {
   }
   const lastOne = async () =>{
     try{
-     
-      // const snap = await getDocs(collection(db, 'debitCredit'));
-      const cred = collection(db, "debitCredit");
-      // console.log(snap[snap.length]    )
-      
-      
-      const q = query(cred, orderBy("time", "desc"), limit(1));
-      console.log(q.data())
-      // q.forEach(doc=>console.log(doc.data()))
+      const snap = await getDocs(collection(db, 'debitCredit'));
+      snap.forEach((doc) => {
+        var data = (doc.id, " => ", doc.data())
+        recArray.push(data);
+      });
+      recArray.forEach(rec=>{
+        comTime.push(rec.time)})
+      comTime.sort()
+      var letest_time = comTime[comTime.length-1]
+
+      recArray.forEach(rec=>{
+        if(rec.time == letest_time)
+          lastRec = rec
+      })
     }
     catch(err){
       console.log(err)
@@ -35,42 +43,44 @@ function CreditDebit() {
   }
 
   const handleSubmitEvent = async (e) =>{
-    lastOne()
-    records.type = type.current.value;
+    await lastOne()
+    console.log(lastRec)
+    records.type = select
     records.subject = subject.current.value;
     records.amount = amount.current.value;
     if(records.type == "debit"){
-      bal = lastRec - records.amount;
+      bal = lastRec.oldBal - Number(records.amount);
     }
-    else{
-      bal = lastRec + records.amount;
+    if(records.type == "credit"){
+      bal = lastRec.oldBal + Number(records.amount);
     }
-    console.log(records.type, records.time)
-    // try{
-    // const docRef = await addDoc(collection(db, 'debitCredit'), {
-    //   Type: records.type,
-    //   subject: records.subject,
-    //   amount: records.amount,
-    //   time: records.time,
-    //   oldBal: bal
-    //   })
-    //   console.log("Document written with ID: ", docRef);
-    // }
-    // catch(err) {
-    //   alert(err)
-    // }
+    records.bal = bal;
+    
+    try{
+    const docRef = await addDoc(collection(db, 'debitCredit'), {
+      Type: records.type,
+      subject: records.subject,
+      amount: Number(records.amount),
+      time: records.time,
+      oldBal: bal
+      })
+      console.log("Document written with ID: ", docRef);
+    }
+    catch(err) {
+      alert(err)
+    }
   }
 
-  
+  // records.type = type.current.value
 
     return (
         <div>
             <form id="debitCredit">
   <div class="form-group">
-    <label for="exampleFormControlSelect1">जमा खर्च होशोब </label>
-    <select class="form-control" id="exampleFormControlSelect1">
-      <option ref={type} name="type" value="debit">खर्च</option>
-      <option ref={type} name="type" value="credit">जमा</option>
+    <label for="exampleFormControlSelect1">जमा खर्च होशोब </label> 
+    <select class="form-control" value={select} onChange={(e)=>{setSelect(e.target.value)}} id="exampleFormControlSelect1">
+      <option name="type" value="debit">खर्च</option>
+      <option name="type" value="credit">जमा</option>
     </select>
   </div>
   <div class="form-group">
@@ -83,7 +93,7 @@ function CreditDebit() {
   </div>
   <button type="button" id="footer" class="btn btn-primary" onClick={(e)=>{
                 e.preventDefault();
-                lastOne()
+                handleSubmitEvent()
             }}>Submit</button>
 </form>
         </div>
